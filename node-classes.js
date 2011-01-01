191d0b576d897a4c1d1619d47f0cbdd42a47546d
Yuya Nishiyama <yuya.nishiyama@gmail.com>
2011-01-02 02:26:19 +0900

    ひとまずモジュール直下のクラス表示まで。

--- node-classes.js
+++ node-classes.js
@@ -1,31 +1,40 @@
 var graphviz = require('graphviz');
+var natives = process.binding('natives');
+var moduleObj, moduleName, classObj, className;
 
 var g = graphviz.digraph('G');
 
 var nodeAttr = {
-  fontname: '"FreeSans.ttf"',
+//  fontname: '"FreeSans.ttf"',
   shape: 'record',
   fontsize: 24
 };
 
-var edgeAttr = {
+var inheritAttr = {
   dir: 'back',
   color: '"#8CACBB"',
   fontsize: 24
 };
 
-g.addNode('EventEmitter', nodeAttr);
-g.addNode('Stream', nodeAttr);
-g.addNode('ReadableStream', nodeAttr);
-g.addNode('WritableStream', nodeAttr);
-g.addNode('Buffer', nodeAttr);
+for (moduleName in natives) {
+  g.addNode(moduleName, nodeAttr);
+  var moduleObj = require(moduleName);
+  //モジュール配下のクラスを検索
+  for (className in moduleObj) {
+    classObj = moduleObj[className];
+    if (typeof classObj === 'function' && isClass(classObj)) {
+      className = '"'+moduleName+'.'+className+'"';
+      g.addNode(className, nodeAttr);
+      g.addEdge(moduleName, className, inheritAttr)
+    }
+  }
+}
 
-g.addEdge('EventEmitter', 'Stream', edgeAttr)
-g.addEdge('Stream', 'ReadableStream', edgeAttr)
-g.addEdge('Stream', 'WritableStream', edgeAttr)
-g.addEdge('ReadableStream', 'Buffer', edgeAttr)
-g.addEdge('WritableStream', 'Buffer', edgeAttr)
+function isClass(classObj) {
+  return classObj.name && classObj.name.match(/^[A-Z].+$/);
+}
+
 
 console.log(g.to_dot());
 
-g.output('png', 'node-gv.png');
+g.output('png', 'node-classes.png');
